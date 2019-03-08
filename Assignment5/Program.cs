7651b057f23371a11f7c6b345f6b0cbcf4723883
XuZiXuan <1830223@AIV.EDMC.STU>
2019-03-07 18:17:41 -0800

    Done

--- Assignment5/Program.cs
+++ Assignment5/Program.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml;
+using System.Xml.Linq;
 using System.Xml.Serialization;
 
 namespace Assignment5
@@ -17,44 +18,67 @@ namespace Assignment5
             Console.WriteLine("Welcome to Assignment 5 - Pokemon Edition");
 
             // TODO: load the pokemon151 xml
+            XElement pokemon151XML = XElement.Load(@"pokemon151.xml");
+            // Console.WriteLine(pokemon151XML);
 
             // TODO: Add item reader and print out all the items
+            Item itemdata = new Item();
+            List<Item> itemlist = new List<Item>();
+            XmlTextReader readerItem = new XmlTextReader("itemData.xml");
+
+            while(readerItem.Read())
+            {
+                
+                if (readerItem.NodeType == XmlNodeType.Element && readerItem.Name == "Name")
+                {
+                    string name = readerItem.ReadElementContentAsString();
+                    itemdata.Name = name;
+                   // Console.WriteLine("Name: " + itemdata.Name);
+                }
+
+                if (readerItem.NodeType == XmlNodeType.Element && readerItem.Name == "UnlockRequirement")
+                {
+                    string UnlockRequirement = readerItem.ReadElementContentAsString();
+                    itemdata.UnlockRequirement = Int32.Parse(UnlockRequirement);
+                   // Console.WriteLine("UnlockRequirement: " + itemdata.UnlockRequirement);
+                }
+
+                if (readerItem.NodeType == XmlNodeType.Element && readerItem.Name == "Description")
+                {
+                    string Description = readerItem.ReadElementContentAsString();
+                    itemdata.Description = Description;
+                   // Console.WriteLine("Description: " + itemdata.Description);
+                }
+
+                if (readerItem.NodeType == XmlNodeType.Element && readerItem.Name == "Effect")
+                {
+                    string Effect = readerItem.ReadElementContentAsString();
+                    itemdata.Effect = Effect;
+                    //Console.WriteLine("Effect: " + itemdata.Effect);
+
+                    itemlist.Add(itemdata);
+                    itemdata = new Item();
+                }
+
+                
+            }
+
+            foreach (Item i in itemlist)
+            {
+                i.Print();
+            }
 
             // TODO: hook up item data to display with the inventory
 
-            var source = new Inventory()
+            Inventory source = new Inventory()
             {
                 ItemToQuantity =
                     new Dictionary<object, object> { { "Poke ball", 10 }, { "Potion", 10 } }
             };
+            
 
             // TODO: move this into a inventory with a serialize and deserialize function.
-            string inventoryFile = "inventory.xml";
-            using (var writer = XmlWriter.Create(inventoryFile))
-                (new XmlSerializer(typeof(Inventory))).Serialize(writer, source);
 
-            using (var reader = new StreamReader(inventoryFile))
-            {
-                var serializer = new XmlSerializer(typeof(Inventory));
-                try
-                {
-                    Inventory inventory = serializer.Deserialize(reader) as Inventory;
-                    if (inventory != null)
-                    {
-                        foreach (var item in inventory.ItemToQuantity)
-                        {
-                            Console.WriteLine("Item: {0} Quantity: {1}", item.Key, item.Value);
-                        }
-                    }
-                }
-
-                catch (Exception ex)
-                {
-                    Console.WriteLine("Cannot load {0} due to the following {1}", 
-                        inventoryFile, ex.Message);
-                }
-
-            }
 
 
             Console.ReadKey();
